c9615e6c80e020df86736f1272cbc9fa972e2976
PabloChacon1997 <pabloandres120297@gmail.com>
2022-06-03 16:55:24 -0500

    Rutas, modelos y controladores

---
+++ controllers/veterinarioController.js
@@ -0,0 +1,14 @@
+const registrar = (req, res) => {
+  const { email, password, nombre } = req.body;
+
+  console.log(nombre);
+  console.log(email);
+  console.log(password);
+  res.json({ msg: "Registrando usuario" });
+};
+
+const perfil = (req, res) => {
+  res.json({ msg: "Mostrando perfil" });
+};
+
+export { registrar, perfil };
